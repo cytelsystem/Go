468d682ea25d8d5d7b7aad184140252bb427e09d
Hector Javier Moreno <cytelsystem@gmail.com>
2023-08-02 12:41:41 -0500

    composicion

--- src/main.go
+++ src/main.go
@@ -1,7 +1,41 @@
 package main
 
+import "fmt"
+
+type Autor struct {
+	Nombre   string
+	Apellido string
+}
+
+func (a Autor) nombreAutor() string {
+	return fmt.Sprintf("%s %s", a.Nombre, a.Apellido)
+}
+
+type Libro struct {
+	Titulo      string
+	Descripcion string
+	Autor       Autor
+}
+
+func (l Libro) informacion() {
+	fmt.Println("Titulo:", l.Titulo)
+	fmt.Println("Descripcion:", l.Descripcion)
+	fmt.Println("Autor:", l.Autor.nombreAutor())
+}
+
 func main() {
 
+	nombreAutor := Autor{
+		Nombre:   "hector",
+		Apellido: "Moreno",
+	}
 
+	libro := Libro{
+		Titulo:      "como Apender go",
+		Descripcion: "clase de dh como aprender go",
+		Autor:       nombreAutor,
+	}
 
- }
+	libro.informacion()
+
+}
