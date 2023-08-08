25e033ad9a648a5b2bbc3b17e14b6f8402e9095d
Hector Javier Moreno <cytelsystem@gmail.com>
2023-08-08 10:05:19 -0500

    manejo de errores

--- src/main.go
+++ src/main.go
@@ -1,57 +1,54 @@
+// ***************************************************************//
+// Error() metodo error de la interface error
+// fmt.Errorf() del paquete fmt
+// Libreria errors permite estas 4 opciones New(), is(), As() y Unwrap(). EJM: errors.New()
+// **cadena de errores
+//	error_1 := fmtErrorf("primer error")
+//	error_2 := fmtErrorf("segundo error %w", error_1)  //incluir el %w para llamar el otro error
+
+// Usamos errors.New() para crear un nuevo error simple y lo asignamos a la variable err.
+// Usamos errors.Is() para verificar si el error err es igual al error que creamos previamente. Si es igual, mostramos un mensaje diciendo que son del mismo tipo.
+// Creamos un error que envuelve al error original utilizando fmt.Errorf(). Usamos la cadena "%w" para indicar que queremos envolver un error en este lugar.
+// Utilizamos errors.Is() nuevamente para verificar si el error envuelto contiene el error original. Si es cierto, mostramos un mensaje.
+// Utilizamos errors.As() para intentar extraer un error relacionado. Si es exitoso, mostramos el mensaje de error relacionado.
+// Utilizamos errors.Unwrap() para obtener el error relacionado sin el envoltorio. Si existe, mostramos el mensaje del error relacionado.
+// Hemos reemplazado el término "subyacente" por "relacionado" para hacer la explicación más clara y comprensible. Este código muestra cómo trabajar con errores en Go utilizando las funciones proporcionadas por la librería errors
+
 package main
 
-import "fmt"
-
-type Producto interface {
-	Precio() float64
-}
-
-type Pequeno struct {
-	precio float64
-}
-
-func (p Pequeno) Precio() float64 {
-	return p.precio
-}
-
-type Mediano struct {
-	precio float64
-}
-
-func (m Mediano) Precio() float64 {
-	return m.precio * 3/100 + m.precio
-}
-
-type Grande struct {
-	precio float64
-}
-
-func (g Grande) Precio() float64 {
-	return g.precio *6 /100 + g.precio + 2500
-}
-
-func CrearProducto(tipo string, precio float64) Producto {
-	switch tipo {
-	case "Pequeno":
-		return Pequeno{precio: precio}
-	case "Mediano":
-		return Mediano{precio: precio}
-	case "Grande":
-		return Grande{precio: precio}
-	default:
-		panic("Tipo de producto no válido")
-
-	}
-
-
-}
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-producto1 := CrearProducto("Pequeno", 100000)
-fmt.Println(producto1.Precio())
-producto2 := CrearProducto("Mediano", 100000)
-fmt.Println(producto2.Precio())
-producto3 := CrearProducto("Grande", 100000)
-fmt.Println(producto3.Precio())
+	// Creamos un nuevo error simple utilizando la función New()
+	err := errors.New("Hubo un problema en la conexión")
 
+	// Verificamos si el error es igual al error específico que creamos antes
+	if errors.Is(err, errors.New("Hubo un problema en la conexión")) {
+		fmt.Println("El error es del mismo tipo")
+	}
+
+	// Creamos un nuevo error que envuelve el error original
+	errorEnvuelto := fmt.Errorf("Ocurrió un problema: %w", err)
+
+	// Verificamos si el error envuelto contiene el error original
+	if errors.Is(errorEnvuelto, err) {
+		fmt.Println("El error envuelto contiene el error original")
+	}
+
+	// Intentamos extraer el error relacionado utilizando As()
+	var errComun error
+	if errors.As(errorEnvuelto, &errComun) {
+		fmt.Println("Error relacionado:", errComun.Error())
+	}
+
+	// Desempaquetamos el error para obtener el error relacionado
+	errRelacionado := errors.Unwrap(errorEnvuelto)
+	if errRelacionado != nil {
+		fmt.Println("Error relacionado desempaquetado:", errRelacionado.Error())
+	}
 }
+
+//***************************************************************//
