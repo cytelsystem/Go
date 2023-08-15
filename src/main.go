19adb8a5c7c88c59a432c295b2f2cb1377512672
Hector Javier Moreno <cytelsystem@gmail.com>
2023-08-15 18:06:23 -0500

    ejercicioCalcularPrecio

--- src/main.go
+++ src/main.go
@@ -1,17 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	n := 3
-	ch := make(chan int)
-	go multiplicarPorDos(n, ch)
-
-	fmt.Println(<-ch)
-
+type Producto struct {
+	nombre   string
+	precio   float64
+	cantidad int
 }
 
-func multiplicarPorDos(num int, ch chan int) {
-	res := num * 2
-	ch <- res
+type Servicio struct {
+	nombre            string
+	precio            float64
+	minutosTrabajados int
+}
+
+type Mantenimiento struct {
+	nombre string
+	precio float64
+}
+
+func sumarProductos(productos []Producto, ch chan<- float64) {
+	total := 0.0
+	for _, p := range productos {
+		total += p.precio * float64(p.cantidad)
+	}
+	ch <- total
+}
+
+func sumarServicios(servicios []Servicio, ch chan<- float64) {
+	total := 0.0
+	for _, s := range servicios {
+		mediaHoraTrabajada := float64(s.minutosTrabajados) / 30.0
+		total += s.precio * mediaHoraTrabajada
+	}
+	ch <- total
+}
+
+func sumarMantenimientos(mantenimientos []Mantenimiento, ch chan<- float64) {
+	total := 0.0
+	for _, m := range mantenimientos {
+		total += m.precio
+	}
+	ch <- total
+}
+
+func main() {
+	productos := []Producto{
+		{"Producto1", 10.0, 3},
+		{"Producto2", 15.0, 2},
+	}
+
+	servicios := []Servicio{
+		{"Servicio1", 5.0, 45},
+		{"Servicio2", 8.0, 20},
+	}
+
+	mantenimientos := []Mantenimiento{
+		{"Mantenimiento1", 50.0},
+		{"Mantenimiento2", 30.0},
+	}
+
+	totalCh := make(chan float64)
+
+	go sumarProductos(productos, totalCh)
+	go sumarServicios(servicios, totalCh)
+	go sumarMantenimientos(mantenimientos, totalCh)
+
+	total := 0.0
+	for i := 0; i < 3; i++ {
+		total += <-totalCh
+	}
+
+	fmt.Printf("Monto final: %.2f\n", total)
 }
