6284a1e3d93485d7ac7194e24e2203f68055c460
Hector Javier Moreno <cytelsystem@gmail.com>
2023-08-15 19:00:42 -0500

    concurencia

--- src/main.go
+++ src/main.go
@@ -4,74 +4,40 @@ import (
 	"fmt"
 )
 
-type Producto struct {
-	nombre   string
-	precio   float64
-	cantidad int
-}
-
-type Servicio struct {
-	nombre            string
-	precio            float64
-	minutosTrabajados int
-}
-
-type Mantenimiento struct {
-	nombre string
-	precio float64
-}
-
-func sumarProductos(productos []Producto, ch chan<- float64) {
-	total := 0.0
-	for _, p := range productos {
-		total += p.precio * float64(p.cantidad)
+func par(c chan int) {
+	for {
+		val := <-c
+		fmt.Printf("Par: %d\n", val)
 	}
-	ch <- total
 }
 
-func sumarServicios(servicios []Servicio, ch chan<- float64) {
-	total := 0.0
-	for _, s := range servicios {
-		mediaHoraTrabajada := float64(s.minutosTrabajados) / 30.0
-		total += s.precio * mediaHoraTrabajada
+func impar(c chan int) {
+	for {
+		val := <-c
+		fmt.Printf("Impar: %d\n", val)
 	}
-	ch <- total
-}
-
-func sumarMantenimientos(mantenimientos []Mantenimiento, ch chan<- float64) {
-	total := 0.0
-	for _, m := range mantenimientos {
-		total += m.precio
-	}
-	ch <- total
 }
 
 func main() {
-	productos := []Producto{
-		{"Producto1", 10.0, 3},
-		{"Producto2", 15.0, 2},
+	paresChan := make(chan int)
+	imparesChan := make(chan int)
+
+	go par(paresChan)
+	go impar(imparesChan)
+
+	numeros := []int{2, 7, 10, 15, 22, 31, 42, 53}
+
+	for _, num := range numeros {
+		if num%2 == 0 {
+			paresChan <- num
+		} else {
+			imparesChan <- num
+		}
 	}
 
-	servicios := []Servicio{
-		{"Servicio1", 5.0, 45},
-		{"Servicio2", 8.0, 20},
-	}
+	close(paresChan)
+	close(imparesChan)
 
-	mantenimientos := []Mantenimiento{
-		{"Mantenimiento1", 50.0},
-		{"Mantenimiento2", 30.0},
-	}
-
-	totalCh := make(chan float64)
-
-	go sumarProductos(productos, totalCh)
-	go sumarServicios(servicios, totalCh)
-	go sumarMantenimientos(mantenimientos, totalCh)
-
-	total := 0.0
-	for i := 0; i < 3; i++ {
-		total += <-totalCh
-	}
-
-	fmt.Printf("Monto final: %.2f\n", total)
+	// Espera indefinida para que las goroutines no terminen prematuramente
+	select {}
 }
