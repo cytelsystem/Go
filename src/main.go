e952a18affd3c6efa72dd6bff7cc3c4e0b39b6ea
Hector Javier Moreno <cytelsystem@gmail.com>
2023-08-16 14:48:17 -0500

    utilizacion de enviroments

--- src/main.go
+++ src/main.go
@@ -4,39 +4,66 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	fileName = "./data.csv"
 )
 
 func main() {
 
-	crearArchivo("./prueba.txt")
-	// readFile("prueba.txt")
+	// Uso del envaironment para setear variables.
+	os.Setenv("usuario", "supervisor")
+
+	usuario := os.Getenv("usuario")
+
+	if usuario == "admin" {
+		readFile(fileName)
+	} else {
+		fmt.Println("Este usuario no es administrador")
+		fmt.Println("Este usuario tiene privilegios de:", usuario)
+	}
 
 }
 
 func readFile(name string) {
+
 	file, err := os.ReadFile(name)
+
 	if err != nil {
-		fmt.Println("Error al leer el archivo")
-		return
+		log.Println("No se pudo leer el archivo")
 	}
 
-	fmt.Println(file)
-}
+	data := strings.Split(string(file), ";")
 
-func crearArchivo(name string) {
-	file, err := os.Create(name)
-	if err != nil {
-		fmt.Println("No se pudo crear el archivo")
-		return
+	var total float64
+	for i := 0; i < len(data)-1; i++ {
+		var line = strings.Split(string(data[i]), ",")
+
+		if i != 0 {
+			precio, err := strconv.ParseFloat(line[1], 64)
+			if err != nil {
+				log.Println("No se pudo parsear el precio")
+			}
+			cantidad, err2 := strconv.ParseFloat(line[2], 64)
+			if err2 != nil {
+				log.Println("No se pudo parsear la cantidad")
+			}
+
+			totalProducto := precio * cantidad
+			total += totalProducto
+		}
+
+		for i := 0; i < len(line); i++ {
+			fmt.Printf("%s\t\t", line[i])
+			if i == len(line)-1 {
+				fmt.Print("\n")
+			}
+		}
 	}
 
-	defer file.Close()
-	texto := "Hola"
+	fmt.Printf("\nTotal\t\t%.2f\n", total)
 
-	_, err = file.WriteString(texto)
-	if err != nil {
-		fmt.Println("no se pudo adicionar el texto")
-	}
-
-	log.Println("Archivo creado correctamente", file, err)
 }
